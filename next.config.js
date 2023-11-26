376708b8101261b63c171f041383fe35d4f05abe
bikboktech <bikboktech@gmail.com>
2023-11-26 08:18:55 +0000

    ci: define env vars via next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+  },
   compiler: {
     styledComponents: true,
   },

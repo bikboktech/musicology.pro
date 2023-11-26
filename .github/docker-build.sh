32e267ab92b3296d33f523f0438132b6a79b79a6
bikboktech <bikboktech@gmail.com>
2023-11-26 09:25:31 +0000

    test: work around environment variables

--- .github/docker-build.sh
+++ .github/docker-build.sh
@@ -3,5 +3,6 @@
 docker build \
     --file ${DOCKERFILE} \
     --tag ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} \
-    # --build-arg NEXT_PUBLIC_API_BASE_URL=${NEXT_PUBLIC_API_BASE_URL} \
     .
+
+# --build-arg NEXT_PUBLIC_API_BASE_URL=${NEXT_PUBLIC_API_BASE_URL} \

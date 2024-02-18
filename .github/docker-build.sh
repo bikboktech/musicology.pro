5365d841cd3281dd047e64547d9e967ce405422c
andhrelja <andhrelja@hotmail.com>
2024-02-18 22:58:53 +0100

    ci: central cicd scripts

--- .github/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-docker build \
-    --file ${DOCKERFILE} \
-    --tag ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} \
-    --build-arg NEXT_PUBLIC_API_BASE_URL=${NEXT_PUBLIC_API_BASE_URL} \
-    --build-arg NEXT_PUBLIC_AUTH_COOKIE_KEY=${NEXT_PUBLIC_AUTH_COOKIE_KEY} \
-    .

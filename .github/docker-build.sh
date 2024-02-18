f514b951dd4ae304c825cc19afaf3832109632be
Andrea Hrelja <34381530+andhrelja@users.noreply.github.com>
2024-02-18 22:56:34 +0100

    fix: update docker-build.sh with env

--- .github/docker-build.sh
+++ .github/docker-build.sh
@@ -4,4 +4,5 @@ docker build \
     --file ${DOCKERFILE} \
     --tag ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} \
     --build-arg NEXT_PUBLIC_API_BASE_URL=${NEXT_PUBLIC_API_BASE_URL} \
+    --build-arg NEXT_PUBLIC_AUTH_COOKIE_KEY=${NEXT_PUBLIC_AUTH_COOKIE_KEY} \
     .
